e5fdbd903ec196d64828ed51472f4bdccbc8d47f
Brian Leung <hyleung111one@yahoo.com>
2021-12-14 15:28:00 +0800

    afternoon commit 5

--- httpserv/src/main/java/server/ass/HttpClientConnection.java
+++ httpserv/src/main/java/server/ass/HttpClientConnection.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -43,7 +44,9 @@ public class HttpClientConnection implements Runnable {
             String dirIn2=delim.next();
             clientIn.add(dirIn2);}
             delim.close();
-            
+        
+            rsName=clientIn.get(1);
+            if(rsName.equals("/")){rsName= "/index.html";}  //rRENAME TO INDEX
        // while (!"close".equals(line) && null != line) {
          //while (true) {
             //System.out.println("Client " + id + ": " + line);           
@@ -57,15 +60,24 @@ public class HttpClientConnection implements Runnable {
                     htpwrite.close();
                     socket.close();
                 } 
-                else if (line.contains("GET")==true) 
-                {  rsName=clientIn.get(1);
-                        if(rsName.equals("//")){rsName= "//index.html";}
-                            if(servCmd.chkPthReadable(clientIn.get(1))==true){  
-                                Path request = Paths.get(clientIn.get(1));
-                            }else
-                            {htpwrite.writeString("HTTP/1.1 405 Method Not Allowed\r\n \r\n" + clientIn.get(1) + "not supported\r\n" ); socket.close();}
-                        
+                else if (servCmd.chkPthReadable(rsName)==false) 
+                {  htpwrite.writeString("HTTP/1.1 405 Method Not Allowed\r\n \r\n" + clientIn.get(1) + "not supported\r\n" ); socket.close();}
+
+                else if (servCmd.chkPthReadable(rsName)==true)
+                {
+                //Path clientRequest = Paths.get(rsName);
+                byte[] bytes = Files.readAllBytes(Paths.get(rsName));
+                htpwrite.writeBytes(bytes);
+                htpwrite.writeString("HTTP/1.1 200 OK\r\n  \r\n " + rsName + " as bytes");
+                socket.close();
                 }
+
+                else if (servCmd.chkPthReadable(rsName)==true && rsName.contains(".png")){
+                    byte[] bytes = Files.readAllBytes(Paths.get(rsName));
+                    htpwrite.writeBytes(bytes);
+                    htpwrite.writeString("HTTP/1.1 200 OK\r\n Content-Type: image//png \r\n" + rsName + " as bytes");
+                    socket.close();  
+                }else{System.out.println("input not read");return;}
 
             } catch (Exception e) {
                 e.printStackTrace();

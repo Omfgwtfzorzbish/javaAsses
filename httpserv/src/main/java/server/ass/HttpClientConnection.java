4d1ef684f5071c9dc0d76245539f2b9c66f0f997
Brian Leung <hyleung111one@yahoo.com>
2021-12-14 14:21:45 +0800

    afternoon commit 3

--- httpserv/src/main/java/server/ass/HttpClientConnection.java
+++ httpserv/src/main/java/server/ass/HttpClientConnection.java
@@ -1,7 +1,56 @@
 package server.ass;
 
+import java.net.Socket;
+import java.util.ArrayList;
+
 public class HttpClientConnection implements Runnable {
-    private final Socket socket;
+    private Socket socket;
     private int id;
-    private String inputFile;
+    private ArrayList<String> inputFile;
+
+    public HttpClientConnection(Socket socket, int id, ArrayList<String> inputFile) {
+        this.socket = socket;
+        this.id = id;
+        this.inputFile = inputFile;
+    }
+    @Override
+    public void run() {
+        PrintWriter out = null;
+        BufferedReader in = null;
+        String line = "";
+        System.out.println("Connection ID: " + id);
+
+        try {
+            out = new PrintWriter(socket.getOutputStream(), true);
+            in = new BufferedReader(
+                new InputStreamReader(socket.getInputStream()));
+            line = in.readLine();
+        } catch (IOException ioe) {
+            System.out.println("Something went wrong..");
+        }
+
+        while (!"close".equals(line) && null != line) {
+
+            System.out.println("Client " + id + ": " + line);
+            
+            try {
+                
+                if ("get-cookie".equals(line)) {
+                    System.out.println("Sending a cookie to client " + id);
+                    out.println("cookie-text " + 
+                            new Cookie().getCookie(inputFile));
+                    out.flush();
+                    line = in.readLine();
+                } else {
+                    out.println("Server: you said " + line);
+                    out.flush();
+                    line = in.readLine();
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                break;
+            } 
+        }
+    }
 }
